f8ad394e84bbd5eac07cd53f902dfc4549932b6d
hideoku <hideoku487@gmail.com>
2015-11-18 02:14:14 +0900

    Initial commit

---
+++ 11_ExcelOperation/MessageId.java
@@ -0,0 +1,20 @@
+package sample.constants;
+
+public class MessageId {
+
+    /** 検索しました。検索結果は{0}件です。 */
+    public static final String MSG001 = "MSG001";
+
+    /** 登録しました。 */
+    public static final String MSG002 = "MSG002";
+
+    /** ユーザIDとパスワードを入力してログインしてください。 */
+    public static final String MSG003 = "MSG003";
+
+    /** {0}は入力必須です。 */
+    public static final String MSG004 = "MSG004";
+
+    /** {0}は{1}以下の数値で入力してください。 */
+    public static final String MSG005 = "MSG005";
+
+}
